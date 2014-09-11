e373351eca0943d98accbb1d51fa431470131926
florije1988 <florije1988@gmail.com>
2014-09-11 19:24:03 +0800

    用户登录表单

---
+++ app/auth/forms.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+__author__ = 'florije'
+
+from flask.ext.wtf import Form
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
+
+
+class LoginForm(Form):
+    email = StringField('Email', validators=[DataRequired(), Length(1, 64), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Keep me logged in')
+    submit = SubmitField('Log In')
